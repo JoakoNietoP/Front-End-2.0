582ee8adf9a416fe539361290b61565293cb7fd1
JoakoNietoP <89438040+JoakoNietoP@users.noreply.github.com>
2021-11-01 20:40:55 -0300

    Clase 5

---
+++ Clase2.5_/00-repasoFunciones.js
@@ -0,0 +1,11 @@
+// function nombreFuncion(parametro) {
+//     //aca lo que pasa
+// }
+
+// //* Mas recomendada: Mas moderna, mejor manejo del this y si es una linea, return implicito
+
+// const nombreFuncion = (parametro) => {
+//     //return aca lo que pasa
+// }
+
+// const nombreFuncion = parametro => //aca lo que pasa y si es una linea. No lleva return
---
+++ Clase2.5_/01-forLoops.js
@@ -0,0 +1,6 @@
+//* https://cathow.dev/javascript-para-bucle/
+//* https://www.freecodecamp.org/espanol/news/javascript-bucles-explicados-for-while-loops/
+//* https://cathow.dev/wp-content/uploads/trabajo-de-bucle-for-en-javascript-con-diagrama-de-flujo.jpg
+
+
+// for (let i = 0; i < 5; i++ )
