59f0401c876c841a77674267dcf84c2782108fbb
JoakoNietoP <89438040+JoakoNietoP@users.noreply.github.com>
2021-11-01 21:45:11 -0300

    Clase 5

--- Clase2.5_/01b-forLoops2.js
+++
@@ -1,7 +0,0 @@
-// //* Mostrar impares
-
-// for (let i = 0; < 15; i++) {
-//     console.log('viene recorriendo: ' + i);
-//     if ( i % 2 !== 0) console.log(i);
-//     //aca es donde hace el incremento. Lo hace
-// }
