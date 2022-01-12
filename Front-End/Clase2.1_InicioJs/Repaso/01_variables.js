d6ca6701f489117529f4133af7bbf49c6f11ea67
Joaquin Nieto <you@example.com>
2022-01-12 06:53:12 -0300

    Repasando con nodejs

--- Front-End/Clase2.1_InicioJs/Repaso/01_variables.js
+++ Front-End/Clase2.1_InicioJs/Repaso/01_variables.js
@@ -1,16 +1,16 @@
+//3 formas de usar las variables
 
+//!Es global, es vieja y ese es su mayor problema. NO USAR
 var primera = "hola mundo";
 
-//Las dos formas que vienen en ES6: ley y const. NO son globales, existen donde hayn sido creadas
+//Las dos formas que vienen del ES6: let const. //!NO SON GLOBALES, EXISTEN DONDE LAS CREAMOS
 
-//const es constante: no se puede cambiar
-
+//const es costante no se puede cambiar
+//! si intentamos cambiarla el codigo se rompe
 const segunda = "hola 2";
 
 //let si puede ser cambiada
-
 let tercera = "hola 3";
 
-//para imprimirlas, usamos console.log()
-
-console.log('primera:', primera);
+//* para imprimir usamos clg() o console.log()
+console.log(primera + " " + tercera + " " + segunda + ' saludamos a medio pais');
