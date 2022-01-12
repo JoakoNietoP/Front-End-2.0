c3512bccfe2309e319e5dca3691012353ed89113
Joaquin Nieto <you@example.com>
2022-01-12 06:18:43 -0300

    agregando

---
+++ Front-End/Clase2.1_InicioJs/Repaso/01_variables.js
@@ -0,0 +1,16 @@
+
+var primera = "hola mundo";
+
+//Las dos formas que vienen en ES6: ley y const. NO son globales, existen donde hayn sido creadas
+
+//const es constante: no se puede cambiar
+
+const segunda = "hola 2";
+
+//let si puede ser cambiada
+
+let tercera = "hola 3";
+
+//para imprimirlas, usamos console.log()
+
+console.log('primera:', primera);
